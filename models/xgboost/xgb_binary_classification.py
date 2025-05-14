bef8d170bda6b69a23a72fca9ee5d2b654dd6647
Gabriel Furtado Teixeira <110139952+GabrielFTgft@users.noreply.github.com>
2025-05-14 20:16:01 -0300

    Update xgb_binary_classification.py

--- models/xgboost/xgb_binary_classification.py
+++ models/xgboost/xgb_binary_classification.py
@@ -37,5 +37,5 @@ orc_model.fit(X_train, y_train)
 y_pred = orc_model.predict(X_test)
 print("Acuracia:", orc_model.score(X_test, y_test))
 
-print(classification_report(y_test, y_pred, zero_division=0))
+print(classification_report(y_test, y_pred))
 

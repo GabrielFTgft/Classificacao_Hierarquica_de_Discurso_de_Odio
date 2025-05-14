e7889f683c92423ae1f63a08091e063a50e91f19
Gabriel Furtado Teixeira <110139952+GabrielFTgft@users.noreply.github.com>
2025-05-14 20:16:21 -0300

    Update xgb_multilabel_classification_orc.py

--- models/xgboost/xgb_multilabel_classification_orc.py
+++ models/xgboost/xgb_multilabel_classification_orc.py
@@ -39,5 +39,5 @@ orc_model.fit(X_train, y_train)
 y_pred = orc_model.predict(X_test)
 print("Shape do y_pred:", y_pred.shape)
 
-print(classification_report(y_test, y_pred, zero_division=0))
+print(classification_report(y_test, y_pred))
 
